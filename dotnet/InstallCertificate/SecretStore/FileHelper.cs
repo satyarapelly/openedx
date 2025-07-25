a7d94971889898a6c5f96c359b55f9d6d810811a
satya rapelly <satyarapelly@users.noreply.github.com>
2025-07-25 00:54:08 -0700

    Add files via upload

---
+++ dotnet/InstallCertificate/SecretStore/FileHelper.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="FileHelper.cs" company="Microsoft">Copyright (c) Microsoft 2013. All rights reserved.</copyright>
+
+namespace Microsoft.Commerce.Payments.Common.Helper
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    internal static class FileHelper
+    {
+        public static async Task<byte[]> ReadAllBytesAsync(string fileName)
+        {
+            using (FileStream fileStream = File.OpenRead(fileName))
+            {
+                byte[] buffer = new byte[fileStream.Length];
+                await fileStream.ReadAsync(buffer, 0, (int)fileStream.Length).ConfigureAwait(false);
+                return buffer;
+            }
+        }
+    }
+}
