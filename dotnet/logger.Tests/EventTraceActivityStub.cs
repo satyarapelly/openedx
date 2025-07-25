2f77ab09185b6a11af431d331f5f7297f723cb3a
satya rapelly <satyarapelly@users.noreply.github.com>
2025-07-25 15:21:42 -0700

    Add SllWebLogger unit test

---
+++ dotnet/logger.Tests/EventTraceActivityStub.cs
@@ -0,0 +1,8 @@
+namespace Microsoft.Commerce.Tracing
+{
+    public struct EventTraceActivity
+    {
+        public static readonly EventTraceActivity Empty = new EventTraceActivity();
+        public override string ToString() => string.Empty;
+    }
+}
