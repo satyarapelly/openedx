b194208873bb104ed8667fed41176ee636caca2c
satya rapelly <satya.rapelly@gmail.com>
2025-08-02 13:38:34 -0700

    Revert "rebase -- master"
    This reverts commit 78c18a0d4fd3aaa809363d9f95ab1c56019edead.

---
+++ net8/migration/PXService.NetStandard/LegacyCommerceService/DataModel/SearchAccountFilters.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="SearchAccountFilters.cs" company="Microsoft">Copyright (c) Microsoft. All rights reserved.</copyright>
+
+namespace Microsoft.Commerce.Payments.PXService.Accessors.LegacyCommerceService.DataModel
+{
+    using System.Runtime.Serialization;
+
+    public class SearchAccountFilters : IExtensibleDataObject
+    {
+        public ExtensionDataObject ExtensionData { get; set; }
+
+        [DataMember]
+        public bool BillableAccountAdminOnly { get; set; }
+
+        [DataMember]
+        public CustomerType? CustomerType { get; set; }
+
+        [DataMember]
+        public string CountryCode { get; set; }
+    }
+}
+
---
+++ net8/migration/PXService.NetStandard/LegacyCommerceService/Messages/GetSubscriptionsResponse.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="GetSubscriptionsResponse.cs" company="Microsoft">Copyright (c) Microsoft. All rights reserved.</copyright>
+
+namespace Microsoft.Commerce.Payments.PXService.Accessors.LegacyCommerceService.Messages
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+    using Microsoft.Commerce.Payments.PXService.Accessors.LegacyCommerceService.DataModel;
+
+    [DataContract(Namespace = NamespaceConstants.Namespace)]
+    public class GetSubscriptionsResponse : AbstractResponse
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly", Justification = "Legacy code")]
+        [DataMember]
+        public List<SubscriptionsInfo> SubscriptionInfoList { get; set; }
+
+        [DataMember]
+        public int TotalCount { get; set; }
+    }
+}
