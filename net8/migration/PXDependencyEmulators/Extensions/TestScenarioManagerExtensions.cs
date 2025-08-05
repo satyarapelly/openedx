647e259781bd7ebed91e75ea0efe8dc6468ac027
satya rapelly <satyarapelly@users.noreply.github.com>
2025-08-04 18:10:02 -0700

    Define scenario manager interfaces

--- net8/migration/PXDependencyEmulators/Extensions/TestScenarioManagerExtensions.cs
+++ net8/migration/PXDependencyEmulators/Extensions/TestScenarioManagerExtensions.cs
@@ -3,8 +3,13 @@ namespace Microsoft.Commerce.Payments.Tests.Emulators.PXDependencyEmulators.Exte
     using Common.Transaction;
     using System.Net.Http;
 
-    public static class ScenarioManagerExtensions
+    public static class TestScenarioManagerExtensions
     {
+        public static HttpResponseMessage GetResponse(this IScenarioManager manager, string apiName)
+        {
+            return manager.GetMockResponse(apiName);
+        }
+
         public static HttpResponseMessage GetResponse(this IScenarioManager manager, string apiName, TestContext testContext)
         {
             return manager.GetMockResponse(apiName, testContext);
