3df932c3b7e55e803c9752f6fda960c14c26f66d
satya rapelly <satyarapelly@users.noreply.github.com>
2025-08-05 16:02:30 -0700

    Refactor PXService test infrastructure for ASP.NET Core

--- net8/migration/COT.PXService/PXServiceClient.cs
+++ net8/migration/COT.PXService/PXServiceClient.cs
@@ -2,8 +2,6 @@
 
 namespace COT.PXService
 {
-    using System;
-    using Newtonsoft.Json;
     using Test.Common;
     
     public class PXServiceClient : ServiceClient
@@ -15,37 +13,5 @@ namespace COT.PXService
         {
         }
 
-        protected override string SerializeObject(object requestInput, string contentType)
-        {
-            if (contentType.ToLower().Contains("application/json"))
-            {
-                return JsonConvert.SerializeObject(requestInput);
-            }
-            else
-            {
-                throw new NotSupportedException(string.Format("{0} is not a supported content type", contentType));
-            }
-        }
-
-        protected override T Deserialize<T>(string value, string contentType)
-        {
-            if (contentType.ToLower().Contains("application/json"))
-            {
-                return JsonConvert.DeserializeObject<T>(value);
-            }
-            else if (contentType.ToLower().Contains(Constants.HeaderValues.HtmlContent))
-            {
-                return (T)Convert.ChangeType(value, typeof(T));
-            }
-            else if (string.IsNullOrEmpty(value))
-            {
-                // The response content is empty when successfully remove PI. Add this to avoid hitting the following "else".
-                return default(T);
-            }
-            else
-            {
-                throw new NotSupportedException(string.Format("{0} is not a supported content type", contentType));
-            }
-        }
     }
 }
