c2ad500244a0c4ae610c6242a4ff8c0a05cb7fdf
satya rapelly <satya.rapelly@gmail.com>
2025-08-28 12:49:28 -0700

    added accessors
    added accessors

---
+++ net8/migration/PXService/Accessors/ShortURLDB/IShortURLDBAccessor.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="IShortURLDBAccessor.cs" company="Microsoft">Copyright (c) Microsoft. All rights reserved.</copyright>
+
+namespace Microsoft.Commerce.Payments.PXService.Accessors.ShortURLDB
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Commerce.Payments.PXService.Model.ShortURLDB;
+    using Microsoft.Commerce.Payments.PXService.Model.ShortURLService;
+    using Microsoft.Commerce.Tracing;
+
+    public interface IShortURLDBAccessor
+    {
+        Task<bool> CheckAndAddCodeEntryAsync(CodeEntry codeEntry);
+
+        Task<CodeEntry> GetCodeEntryAsync(string code);
+
+        Task UpdateCodeEntryAsync(CodeEntry codeEntry);
+
+        Task<CreateResponse> Create(CreateRequest request);
+    }
+}
